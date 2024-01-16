87ede758a02104774776b1e8e5ee4e56ab386c8f
Rohit Kumar <rohit.jit76@gmail.com>
2024-01-16 11:00:43 +0700

    index.html replaced insted of home.htm

--- cal.js
+++ cal.js
@@ -1,7 +1,6 @@
 
 //finction For the calculating Data
-function cal()
-{
+function cal() {
 
 
     var salamount = parseInt(document.emform.sal.value);
@@ -9,22 +8,21 @@ function cal()
     var daamount = parseInt(document.emform.DAP.value);
     var hraamount = parseInt(document.emform.HRAP.value);
     var taxamount = parseInt(document.emform.tax1.value);
-    TAAM =salamount*taamount/100;
-    DAAM =salamount*daamount/100;
-    HRAM =salamount*hraamount/100;
-    TAXAM =salamount*taxamount/100;
-    gam=DAAM+HRAM+TAAM;;
+    TAAM = salamount * taamount / 100;
+    DAAM = salamount * daamount / 100;
+    HRAM = salamount * hraamount / 100;
+    TAXAM = salamount * taxamount / 100;
+    gam = DAAM + HRAM + TAAM;;
 
-    document.emform.DA.value=DAAM; 
-    document.emform.HRA.value=HRAM; 
-    document.emform.TA.value=TAAM; 
-    document.emform.GS.value= gam;
-    document.emform.tax.value=TAXAM;
-    document.emform.NS.value=salamount-(gam+TAXAM);
+    document.emform.DA.value = DAAM;
+    document.emform.HRA.value = HRAM;
+    document.emform.TA.value = TAAM;
+    document.emform.GS.value = gam;
+    document.emform.tax.value = TAXAM;
+    document.emform.NS.value = salamount - (gam + TAXAM);
 }
 
-function act()
-{
+function act() {
     window.close();
-    window.open("home.htm");
+    window.open("index.htm");
 }
